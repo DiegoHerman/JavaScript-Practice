2b0d6473eee540082c9e323cc28f4cd91745feb7
Diego <diegovenon456@gmail.com>
2026-02-03 19:16:44 +0300

    applying transition to an svg

--- Randoms/random.js
+++ Randoms/random.js
@@ -1,2 +1,11 @@
-const myNotification = new Notification("Hello!");
-console.log(myNotification);
+const html=document.querySelector("html");
+const circle2=document.querySelector(".circle2");
+const svg=document.querySelector("svg");
+
+html.addEventListener("click",(e)=>{
+    const position=svg.getBoundingClientRect();
+    const cx=e.clientX-position.left;
+    const cy=e.clientY-position.top;
+    circle2.setAttribute("cx",cx);
+    circle2.setAttribute("cy",cy);
+})
