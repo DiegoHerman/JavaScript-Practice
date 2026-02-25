f1d32413218e0bbf45caf4af055b615ee18ca2f5
Diego <diegovenon456@gmail.com>
2026-02-26 01:19:08 +0300

    practice on using json, fetch and promises

--- Randoms/random.js
+++ Randoms/random.js
@@ -1,11 +1,48 @@
-const html=document.querySelector("html");
-const circle2=document.querySelector(".circle2");
-const svg=document.querySelector("svg");
+const body=document.querySelector("body");
+const par1=document.createElement("p");
+const par2=document.createElement("p");
 
-html.addEventListener("click",(e)=>{
-    const position=svg.getBoundingClientRect();
-    const cx=e.clientX-position.left;
-    const cy=e.clientY-position.top;
-    circle2.setAttribute("cx",cx);
-    circle2.setAttribute("cy",cy);
-})
+async function displayCatInfo(){
+    let cats;
+    try{
+    const response=await fetch("https://raw.githubusercontent.com/mdn/learning-area/refs/heads/main/javascript/oojs/tasks/json/sample.json");
+    if(!response.ok){
+        throw new Error(`https error: ${response.status}`);
+    }
+    cats=await response.json();
+    console.log(cats[0].name);
+    }catch(error){
+        console.error(`fetch request failed: ${error}`);
+    }
+
+    const catLength=cats.length;
+    par1.textContent="The mothers names are ";
+    cats.forEach((cat)=>{
+        console.log(cat.name);
+        par1.textContent+=cat.name;
+        if(cat==cats[catLength-2]){
+            par1.textContent+=" and ";
+            return;
+        }
+        if(cat==cats[catLength-1]){
+            return;
+        }
+        par1.textContent+=",";       
+    });
+    body.appendChild(par1);
+
+    let maleKittens=0;
+    let femaleKittens=0;
+    let totalKittens=0;
+    par2.textContent="The total number of kittens are"
+    cats.forEach((cat)=>{
+        cat.kittens.forEach((kitten)=>{
+            kitten.gender==="m"?maleKittens++:femaleKittens++;
+        })
+    });
+    totalKittens=maleKittens+femaleKittens;
+    par2.textContent=`The total number of kittens is ${totalKittens}, ${maleKittens} male kittens and ${femaleKittens} female kittens`;
+    body.appendChild(par2);
+}
+
+displayCatInfo();
